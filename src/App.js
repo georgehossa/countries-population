3fec8fa423598887bb8a8375df926fe4a2cce44d
Jorge Ossa <jorgehossa@gmail.com>
2020-10-15 00:34:37 -0500

    Test data render in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './utils/data.json';
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          data.map(country => <p key={country.id}>{country.name} - {country.population}</p>)
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
