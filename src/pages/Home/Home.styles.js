1b1e3d3daa443846b1d0646555bb844809067a3a
Jorge Ossa <jorgehossa@gmail.com>
2020-10-17 00:48:48 -0500

    Add Home component and configure globalStyles

---
+++ src/pages/Home/Home.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  display: flex;
+  width:100%;
+  height: 100vh;
+  background: peru;
+  margin: 0;
+  padding: 0;
+`;
