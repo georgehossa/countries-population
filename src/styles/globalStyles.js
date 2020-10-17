082f12a92da8e95499d065c22bbfee7d57473ef4
Jorge Ossa <jorgehossa@gmail.com>
2020-10-17 02:27:15 -0500

    Add app font family

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
+    font-family: 'IBM Plex Sans', sans-serif;
   }
 `;
